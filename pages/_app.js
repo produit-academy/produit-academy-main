53234cc705f18fa8be83178de4a4c6523b007c33
ras-al <rasalkpk66@gmail.com>
2026-03-06 00:16:59 +0530

    initial_commit_comming soon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import Head from 'next/head';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Produit Academy | Magic in Progress ✨</title>
+        <meta name="description" content="Produit Academy is building the future of education." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
